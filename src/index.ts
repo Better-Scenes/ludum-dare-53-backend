cbaced14f9b0539a5be6c0d4c073dc33b261d7c4
Rohan Richards <16222002+rohanrichards@users.noreply.github.com>
2023-05-01 09:10:12 +1000

    bind onto port

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import * as readline from 'readline';
 import { WebSocketServer } from './websocket';
 import GameState from './gamestate';
 
-const websocketPort = 8080;
-const websocketServer = new WebSocketServer(websocketPort);
+const port = parseInt(process.env.PORT || '8080'); // Fallback to 3000 if running locally
+const websocketServer = new WebSocketServer(port);
 
 
 const scenarios = [
