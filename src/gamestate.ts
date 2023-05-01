bd3a33444ef6600dc0118875ad936458332617de
Rohan Richards <16222002+rohanrichards@users.noreply.github.com>
2023-05-01 12:08:52 +1000

    select a random prompt

--- src/gamestate.ts
+++ src/gamestate.ts
@@ -20,10 +20,24 @@ export type GameData = {
 class GameStateClass {
 
     private games: Map<string, GameData> = new Map();
+
+    private prompts =         [
+        "You are a strong caveowoman, and you are explaining to your boyfriend that you are pregnant",
+        "You are a clumsy waiter, and you are apologizing to a customer for spilling their drink",
+        "You are a nervous teenager, and you are trying to ask your crush to prom",
+        "You are a paranoid librarian, and you are accusing a visitor of stealing a book",
+        "You are an overenthusiastic gym instructor, and you are motivating a reluctant client during their workout",
+        "You are a forgetful chef, and you are trying to remember the ingredients of a dish while a sous-chef listens",
+        "You are a lost tourist, and you are asking a local for directions to a famous landmark",
+        "You are a talkative hairdresser, and you are sharing gossip with a client while cutting their hair",
+        "You are a mischievous dog, and you are trying to convince your owner to give you a treat",
+        "You are a concerned neighbor, and you are informing the tenant next door about a possible gas leak",
+        "You are a persistent salesperson, and you are trying to sell an uninterested customer a vacuum cleaner"
+      ]
     
     public startNewGame(uuid: string) {
         //get or generate prompt
-        const prompt = "You are a cavewoman, you are trying to explain to your boyfriend that you are pregnant"
+        const prompt = this.prompts[Math.floor(Math.random() * this.prompts.length)]
         const gameData: GameData = {
             prompt,
             messages: []
--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,3 @@ process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled promise rejection:', reason);
     // Add any additional error handling or logging you need
 });
-
-  
-
-const scenarios = [
-    `A couple has just had a heated argument, and the woman is now tearfully pleading with her partner to forgive her and take her back.`
-    , `Two former friends who have been estranged for years run into each other unexpectedly, and one of them delivers a tearful monologue about how much they miss their friendship and how sorry they are for the past.`
-    , `A parent and child have a tense confrontation in which the child confesses to a serious wrongdoing, and the parent responds with angry tears and a desperate attempt to understand why their child would do such a thing.`
-    , `A couple is facing a crisis, and one of them delivers a dramatic, tearful monologue about how much they love the other person and how they will do whatever it takes to save their relationship.`
-    , `Two siblings who have always been close are having a tearful goodbye as one of them prepares to leave home for college or another major life event.`
-    , `A character is confronted by a painful truth or realization, and they break down in tears as they struggle to come to terms with it.`
-    , `Two characters are in the midst of a heated argument, and one of them suddenly breaks down in tears, revealing their vulnerability and begging for forgiveness.`
-    , `A character receives devastating news, and they break down in tears as they struggle to cope with the sudden loss or change.`
-];
